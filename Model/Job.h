9fd002b03b0ee4ba64a5f64a8bc2d009ae5a59bb
ksrcr7 <kasrabakhshaei25@gmail.com>
2026-04-28 12:56:05 +0330

    Refactor: make enum class JobStatus public

--- Model/Job.h
+++ Model/Job.h
@@ -9,18 +9,20 @@ namespace backend{
     struct Payload{std::string description;};
 
     class Job {
+    public:
         enum class JobStatus{Pending,Running,Done,Failed};
+        Job(int jobId,const Payload&  p);
+        [[nodiscard]] int getId()const;
+        [[nodiscard]] JobStatus getStatus()const;
+        [[nodiscard]] const Payload& getPayload() const;
+        void setStatus(JobStatus jobStatus);
+
     private:
         Payload payload;
         int id;
         JobStatus status;
 
-    public:
-        Job(int num,const Payload&  p);
-        [[nodiscard]] int getId()const;
-        [[nodiscard]] JobStatus getStatus()const;
-        [[nodiscard]] const Payload& getPayload() const;
-        void setStatus(JobStatus jobStatus);
+
 
 
 
