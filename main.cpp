23105c1d222353181a85753821818514112c7b2e
ksrcr7 <kasrabakhshaei25@gmail.com>
2026-04-29 16:37:03 +0330

    feat: complete end-to-end job processing system

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
-#include <iostream>
+#include "Worker/Worker.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    backend::TaskQueue queue;
+    backend::Worker worker(queue);
+
+    backend::Payload pay1("Buy Milk");
+    backend::Job job1(1,pay1);
+    backend::Payload pay2("Buy Pc");
+    backend::Job job2(2,pay2);
+    backend::Payload pay3("Buy Laptop");
+    backend::Job job3(3,pay3);
+
+    queue.push(std::make_unique<backend::Job>(job1));
+    queue.push(std::make_unique<backend::Job>(job2));
+    queue.push(std::make_unique<backend::Job>(job3));
+
+    worker.processNextJob();
+    worker.processNextJob();
+    worker.processNextJob();
+
+
+
 }
